e64343cbccbe4e77a62d071bdb4d142bc31a4c88
Kovalan R <kovalan@exotel.in>
2018-12-15 12:31:32 +0530

    [helperFunc] Adding function to get list of IP's which are connected in the server

---
+++ helper/helper.go
@@ -0,0 +1,31 @@
+package helper
+
+import (
+	"fmt"
+	"net"
+)
+
+// List all the Ip's which are present in server.
+func GetIps() []string {
+	ifaces, err := net.Interfaces()
+	if err != nil {
+		fmt.Print(fmt.Errorf("Error getting IP addresses: %+v\n", err.Error()))
+		return nil
+	}
+	var IPs []string
+	for _, ip := range ifaces {
+		addrs, err := ip.Addrs()
+		if err != nil {
+			fmt.Print(fmt.Errorf("Error fetching IP Addresses: %+v\n", err.Error()))
+			continue
+		}
+		for _, addr := range addrs {
+			if ipnet, ok := addr.(*net.IPNet); ok && !ipnet.IP.IsLoopback() {
+				if ipnet.IP.To4() != nil {
+					IPs = append(IPs, ipnet.IP.String())
+				}
+			}
+		}
+	}
+	return IPs
+}
