b178f7837b12a9a67edcf349870f2212bcbb4396
Govind <naangovind@gmail.com>
2019-02-25 08:23:40 +0530

    Monitoring module: Removing unwanter package/methods.
    - i.e removing pinger package and reusing existing one.
    - Removing some methods from monitor package which will not be in use.

--- monitor/monitor.go
+++ monitor/monitor.go
@@ -1,9 +1,7 @@
 package monitor
 
 import (
-	"fmt"
-	"log"
-	"net"
+	"errors"
 	"os"
 	"os/signal"
 	"sync"
@@ -12,7 +10,7 @@ import (
 
 	"github.com/govindarajan/laserproxy/helper"
 	"github.com/govindarajan/laserproxy/logger"
-	"github.com/govindarajan/laserproxy/pinger"
+	pinger "github.com/sparrc/go-ping"
 )
 
 var statsChan chan *pinger.Statistics
@@ -40,7 +38,7 @@ func (m *Monitor) Schedule(seconds int) {
 	for {
 		select {
 		case <-interval.C:
-			m.Run()
+			// TODO: Schedule check
 		case <-osChan:
 			close(m.done)
 			return
@@ -56,84 +54,6 @@ func (m *Monitor) Schedule(seconds int) {
 func GetMonitorResults() (result Results, err error) {
 
 	return Results{Interfaces: []string{fastestlocalIP}, TargetIPs: []string{}}, nil
-}
-
-func (m *Monitor) collectResults() {
-	minPktLoss := 1000.00
-	defer m.wtGroup.Done()
-	for stat := range statsChan {
-		if _, ok := localIfc[stat.Addr]; ok {
-			//this is a local interface
-			if stat.PacketsLoss < minPktLoss {
-				minPktLoss = stat.PacketsLoss
-				fastestlocalIP = stat.Addr
-			}
-		}
-		//fmt.Println(stat)
-	}
-	fmt.Println(fastestlocalIP, "is the fastest local IP")
-}
-
-//Run runs the monitors on the interfaces and target ips
-func (m *Monitor) Run() {
-	localIfc = make(map[string]bool)
-	//todo: Get from  package store here
-	localRoutes := getLocalInterfaces()
-	fmt.Println(localRoutes)
-	targetRoutes := getTargetInterfaces()
-	statsChan = make(chan *pinger.Statistics, len(localRoutes)+len(targetRoutes))
-	var wg sync.WaitGroup
-	wg.Add(2)
-	m.wtGroup.Add(1)
-	go m.collectResults()
-	//collect stats  for  local  interface routes
-	//or other gateway routes
-	go func(wtGrp *sync.WaitGroup, routes []string) {
-		defer wtGrp.Done()
-		collectStatistics(routes, true)
-	}(&wg, localRoutes)
-
-	//collect  stats  for  target routes
-	go func(wtGrp *sync.WaitGroup, routes []string) {
-		defer wtGrp.Done()
-		collectStatistics(routes, false)
-	}(&wg, targetRoutes)
-	wg.Wait()
-	close(statsChan)
-	m.wtGroup.Wait()
-
-}
-
-func collectStatistics(routes []string, isLocalInterface bool) {
-	var wg sync.WaitGroup
-	for _, route := range routes {
-		wg.Add(1)
-		go func(wtGrp *sync.WaitGroup, ipRoute string) {
-			defer wtGrp.Done()
-			pong, err := pinger.NewPinger(ipRoute)
-			if err != nil {
-				log.Println("unable to ping ip ", ipRoute)
-				return
-			}
-			if isLocalInterface {
-				pong.Source = ipRoute
-				ipaddr, _ := net.ResolveIPAddr("ip", "8.8.8.8")
-				pong.IPAddr = ipaddr
-				localIfc[ipRoute] = true
-			}
-			pong.Count = 100
-			pong.Timeout = time.Second * 25
-			pong.Interval = time.Second / 100
-			err = pong.Run()
-			if err != nil {
-				log.Println("unable to collect stats for ip ", ipRoute, err)
-				return
-			}
-			stats := pong.Statistics()
-			statsChan <- stats
-		}(&wg, route)
-	}
-	wg.Wait()
 }
 
 //package store
@@ -153,4 +73,24 @@ func getLocalInterfaces() []string {
 
 func getTargetInterfaces() []string {
 	return []string{"8.8.8.8", "github.com", "8.8.4.4"}
+}
+
+const PACKET_COUNT = 10
+const TIMEOUT_IN_SEC = 2
+const INTERVAL_IN_MS = 100
+
+func GetPingStats(addr string) (*pinger.Statistics, error) {
+	if addr == "" {
+		return nil, errors.New("Address is empty")
+	}
+	ping, err := pinger.NewPinger(addr)
+	if err != nil {
+		return nil, err
+	}
+	ping.Count = PACKET_COUNT
+	ping.Interval = INTERVAL_IN_MS * time.Millisecond
+	ping.Timeout = TIMEOUT_IN_SEC * time.Second
+	ping.SetPrivileged(true)
+	ping.Run()
+	return ping.Statistics(), nil
 }
